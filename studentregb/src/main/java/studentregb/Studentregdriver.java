de5a837cb42536096e84e7602c605d8ea2dc367e
Liam Phillips <L.PHILLIPS3@nuigalway.ie>
2017-10-13 22:18:25 +0100

    StudentRegb

--- studentregb/src/main/java/studentregb/Studentregdriver.java
+++ studentregb/src/main/java/studentregb/Studentregdriver.java
@@ -12,39 +12,39 @@ import org.joda.time.LocalDate;
 public class Studentregdriver {
     
     public static void main(String args[]){
-        //define student
+        //Defines students
         Student s1 = new Student("Grant Gustin","14/01/1990",27,"BLE");
         Student s2 = new Student("Chris Rock","07/02/1965",52,"BLE");
         Student s3 = new Student("Post Malone","04/07/1995",22,"MEE");
         Student s4 = new Student("Hugh Jackman","12/10/1968",49,"MEE");
         Student s5 = new Student("Kendrick Lamar","17/06/1987",30,"BP");
         
-        //all students
-        ArrayList<Student> group1 = new ArrayList<Student>();
-        group1.add(s1);
-        group1.add(s2);
-        group1.add(s3);
-        group1.add(s4);
-        group1.add(s5);
+        //Adds all students
+        ArrayList<Student> groupa = new ArrayList<Student>();
+        groupa.add(s1);
+        groupa.add(s2);
+        groupa.add(s3);
+        groupa.add(s4);
+        groupa.add(s5);
         
-        //BLE and MEE
-        ArrayList<Student> group2 = new ArrayList<Student>();
-        group2.add(s1);
-        group2.add(s2);
-        group2.add(s3);
-        group2.add(s4);
+        //Adds BLE and MEE students
+        ArrayList<Student> groupb = new ArrayList<Student>();
+        groupb.add(s2);
+        groupb.add(s3);
+        groupb.add(s4);
+        groupb.add(s5);
         
-        //MEE and BP
-        ArrayList<Student> group3 = new ArrayList<Student>();
-        group3.add(s3);
-        group3.add(s4);
-        group3.add(s5);
+        //Adds MEE and BP students
+        ArrayList<Student> groupc = new ArrayList<Student>();
+        groupc.add(s1);
+        groupc.add(s2);
+        groupc.add(s3);
         
-        //Module Definitions
-        Module CT420 = new Module("Real Time Systems","CT420",group1);
-        Module CT414 = new Module("Distributed Systems","CT414",group2);
-        Module EE444 = new Module("Signal Processing","EE444",group3);
-        Module EE453 = new Module("Telecommunications","EE453",group3);
+        //Defines modules
+        Module CT420 = new Module("Real Time Systems","CT420",groupa);
+        Module CT414 = new Module("Distributed Systems","CT414",groupb);
+        Module EE444 = new Module("Signal Processing","EE444",groupc);
+        Module EE453 = new Module("Telecommunications","EE453",groupc);
         
         //BLE Modules
         ArrayList<Module> BLEmodules = new ArrayList<Module>();
@@ -70,7 +70,7 @@ public class Studentregdriver {
         Programme MEE = new Programme("MEE",MEEmodules,start,end);
         
                
-        //Prints Student name, Course & modules
+        //Prints Student name, course and modules
         BLE.printStudents();
         MEE.printStudents();
         BP.printStudents();
