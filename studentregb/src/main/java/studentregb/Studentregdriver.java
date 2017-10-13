975bad185de30ae7b456fc91a30227cc6fab88b1
Liam Phillips <L.PHILLIPS3@nuigalway.ie>
2017-10-13 21:28:38 +0100

    StudentRegb

--- studentregb/src/main/java/studentregb/Studentregdriver.java
+++ studentregb/src/main/java/studentregb/Studentregdriver.java
@@ -13,11 +13,11 @@ public class Studentregdriver {
     
     public static void main(String args[]){
         //define student
-        Student s1 = new Student("Grant Gustin","14/01/1990",27,"CSIT");
-        Student s2 = new Student("Chris Rock","07/02/1965",52,"CSIT");
-        Student s3 = new Student("Post Malone","04/07/1995",22,"BP");
-        Student s4 = new Student("Hugh Jackman","12/10/1968",49,"BP");
-        Student s5 = new Student("Kendrick Lamar","17/06/1987",30,"EE");
+        Student s1 = new Student("Grant Gustin","14/01/1990",27,"BLE");
+        Student s2 = new Student("Chris Rock","07/02/1965",52,"BLE");
+        Student s3 = new Student("Post Malone","04/07/1995",22,"MEE");
+        Student s4 = new Student("Hugh Jackman","12/10/1968",49,"MEE");
+        Student s5 = new Student("Kendrick Lamar","17/06/1987",30,"BP");
         
         //all students
         ArrayList<Student> group1 = new ArrayList<Student>();
@@ -27,55 +27,55 @@ public class Studentregdriver {
         group1.add(s4);
         group1.add(s5);
         
-        //CS&IT and BP
+        //BLE and MEE
         ArrayList<Student> group2 = new ArrayList<Student>();
         group2.add(s1);
         group2.add(s2);
         group2.add(s3);
         group2.add(s4);
         
-        //BP and EE
+        //MEE and BP
         ArrayList<Student> group3 = new ArrayList<Student>();
         group3.add(s3);
         group3.add(s4);
         group3.add(s5);
         
         //Module Definitions
-        Module CT417 = new Module("Software Eng. 3","CT417",group1);
-        Module CT401 = new Module("Machine Learning","CT401",group2);
-        Module EE445 = new Module("DSP","EE445",group3);
-        Module EE451 = new Module("System on Chip 1","EE451",group3);
+        Module CT420 = new Module("Real Time Systems","CT420",group1);
+        Module CT414 = new Module("Distributed Systems","CT414",group2);
+        Module EE444 = new Module("Signal Processing","EE444",group3);
+        Module EE453 = new Module("Telecommunications","EE453",group3);
         
-        //CS Modules
-        ArrayList<Module> CSmodules = new ArrayList<Module>();
-        CSmodules.add(CT417);
-        CSmodules.add(CT401);
-        LocalDate start = new LocalDate(2016,9,5);
-        LocalDate end = new LocalDate(2017,5,31);
-        Programme CSIT = new Programme("CSIT",CSmodules,start,end);
+        //BLE Modules
+        ArrayList<Module> BLEmodules = new ArrayList<Module>();
+        BLEmodules.add(CT420);
+        BLEmodules.add(CT414);
+        LocalDate start = new LocalDate(2017,10,13);
+        LocalDate end = new LocalDate(2018,5,31);
+        Programme BLE = new Programme("BLE",BLEmodules,start,end);
         
         //BP Modules
         ArrayList<Module> BPmodules = new ArrayList<Module>();
-        BPmodules.add(EE445);
-        BPmodules.add(CT417);
-        BPmodules.add(CT401);
-        BPmodules.add(EE451);
+        BPmodules.add(EE453);
+        BPmodules.add(EE444);
+        BPmodules.add(CT414);
         Programme BP = new Programme("BP",BPmodules,start,end);
         
-        //EE Modules
-        ArrayList<Module> EEmodules = new ArrayList<Module>();
-        EEmodules.add(EE451);
-        EEmodules.add(EE445);
-        EEmodules.add(CT401);
-        Programme EE = new Programme("EE",EEmodules,start,end);
+        //MEE Modules
+        ArrayList<Module> MEEmodules = new ArrayList<Module>();
+        MEEmodules.add(EE444);
+        MEEmodules.add(CT420);
+        MEEmodules.add(CT414);
+        MEEmodules.add(EE453);
+        Programme MEE = new Programme("MEE",MEEmodules,start,end);
         
+               
         //Prints Student name, Course & modules
-        CSIT.printStudents();
+        BLE.printStudents();
+        MEE.printStudents();
         BP.printStudents();
-        EE.printStudents();
     
        
         
     }
-    
 }
